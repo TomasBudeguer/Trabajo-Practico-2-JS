d7ab0098364c41a44304d797ff51924b270d1026
Tomas <tomibudeguer20@gmail.com>
2022-07-05 20:49:08 -0300

    hice el ejercicio 1

---
+++ js/ejercicio1.js
@@ -0,0 +1,6 @@
+let edad = parseInt(prompt('Ingrese su edad: '))
+if(edad > 18){
+    document.write('Si puede conducir')
+}else{
+    document.write('No puede conducir, introduzca un numero valido')
+}
