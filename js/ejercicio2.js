6cc2d7fe007eaf1a702b56a1cd51c9b60631f8e6
Tomas <tomibudeguer20@gmail.com>
2022-07-07 23:03:00 -0300

    hice el ejercicio 2

---
+++ js/ejercicio2.js
@@ -0,0 +1,29 @@
+let nota = parseInt(prompt('Ingrese su nota del 1 al 10'))
+
+switch(nota){
+    case 0:
+    case 1:
+    case 2:
+        alert('Muy deficiente');
+    break;
+    case 3:
+    case 4:
+        alert('Insuficiente');
+    break;
+    case 5:
+    case 6:
+        alert('Suficiente');
+    break;
+    case 7:
+        alert('Bien');
+    break;
+    case 8:
+    case 9:
+        alert('Notable');
+    break;
+    case 10:
+        alert('Sobresaliente');
+    break;
+    default:
+            alert('Ingrese una nota del 1 al 10')
+}
