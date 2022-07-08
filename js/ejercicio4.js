b73a5faa9b0b1134a09cb5f1a982c22085f6b755
Tomas <tomibudeguer20@gmail.com>
2022-07-08 13:14:45 -0300

    hice el ejercicio 4

---
+++ js/ejercicio4.js
@@ -0,0 +1,10 @@
+let suma = 0;
+do {
+  let numero = parseInt(prompt("Introduce un numero"));
+  if ((numero = Number(numero))) {
+    suma = suma + numero;
+  } else if (numero != Number(numero)) {
+    alert("El valor ingresado NO es un numero");
+  }
+} while (confirm("¿Desea seguir escribiendo numeros?"));
+document.write(suma);
