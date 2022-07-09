da1f5ce96772c40673dde5576348650c2323931d
Tomas <tomibudeguer20@gmail.com>
2022-07-08 22:42:25 -0300

    hice el ejercicio 16

---
+++ js/ejercicio16.js
@@ -0,0 +1,7 @@
+let frase = prompt("Introduce un Texto:");
+let reves = "";
+for (let i = 0; i < frase.length; i++) {
+    let caracter = frase.charAt(i);
+    reves = caracter + reves;
+}
+document.write(salida);
