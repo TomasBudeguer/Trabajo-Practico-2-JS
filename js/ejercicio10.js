985e24b647a67fd7ba079501bef5ca972cb57407
Tomas <tomibudeguer20@gmail.com>
2022-07-12 19:56:33 -0300

    hice el ejercicio 10

---
+++ js/ejercicio10.js
@@ -0,0 +1,14 @@
+const filas = parseInt(prompt("ingrese un numero de filas"));
+const columnas = parseInt(prompt("ingrese un numero de columnas"));
+let resultado = filas * columnas;
+document.write(`<table> <tbody>`);
+for(let indiceFila = 0; indiceFila < filas; indiceFila++){
+    document.write(`<tr>`)
+    for(let indiceColumna = 0; indiceColumna < columnas;indiceColumna++){
+        document.write(`<td>${resultado}</td>`);
+        resultado--;
+    }
+    document.write(`</tr>`);
+}
+
+document.write(`</tbody> </table>`);
